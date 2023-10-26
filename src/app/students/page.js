f4184d32725339b0a90120f40b1f2a53e01e524a
Duong Duc Trung <trungyna1708@gmail.com>
2023-10-26 20:50:58 +0700

    initial commit

--- src/app/students/page.js
+++ src/app/students/page.js
@@ -48,15 +48,15 @@ export default function Students() {
     router.push(`/students/${id}`);
   };
 
-  const getGender = (value) => {
-    if (value === "M") {
-      return "Male";
-    }
-    if (value === "F") {
-      return "Female";
-    }
-    return "";
-  };
+  // const getGender = (value) => {
+  //   if (value === "M") {
+  //     return "Male";
+  //   }
+  //   if (value === "F") {
+  //     return "Female";
+  //   }
+  //   return "";
+  // };
 
   const searchStudents = async () => {
     try {
@@ -110,35 +110,35 @@ export default function Students() {
       <div className="bg-gradient-to-r from-black to-white text-white min-h-screen">
         <div className="container mx-auto py-4">
           <div className="text-2xl font-bold text-align-center">Students</div>
-          <section class="flex items-center w-full">
-            <div class="w-full">
-              <div class="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
-                <div class="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
-                  <div class="w-full md:w-1/2">
-                    <form class="flex items-center">
-                      <label for="simple-search" class="sr-only">
+          <section className="flex items-center w-full">
+            <div className="w-full">
+              <div className="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+                <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+                  <div className="w-full md:w-1/2">
+                    <form className="flex items-center">
+                      <label htmlFor="simple-search" className="sr-only">
                         Search
                       </label>
-                      <div class="relative w-full">
-                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <div className="relative w-full">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                           <svg
                             aria-hidden="true"
-                            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                            className="w-5 h-5 text-gray-500 dark:text-gray-400"
                             fill="currentColor"
-                            viewbox="0 0 20 20"
+                            viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg"
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                              clip-rule="evenodd"
+                              clipRule="evenodd"
                             />
                           </svg>
                         </div>
                         <input
                           type="text"
                           id="simple-search"
-                          class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                          className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                           placeholder="Search"
                           required=""
                           value={filters.searchTerm}
@@ -152,22 +152,22 @@ export default function Students() {
                       </div>
                     </form>
                   </div>
-                  <div class="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
+                  <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
                     <AppButton
                       onClick={createNew}
                       type="button"
-                      class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-gray-800 hover:bg-gray-800 focus:ring-4 focus:ring-primary-300 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-primary-800"
+                      className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-gray-800 hover:bg-gray-800 focus:ring-4 focus:ring-primary-300 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-primary-800"
                     >
                       <svg
-                        class="h-3.5 w-3.5 mr-2"
+                        className="h-3.5 w-3.5 mr-2"
                         fill="currentColor"
                         viewbox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg"
                         aria-hidden="true"
                       >
                         <path
-                          clip-rule="evenodd"
-                          fill-rule="evenodd"
+                          clipRule="evenodd"
+                          fillRule="evenodd"
                           d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
                         />
                       </svg>
@@ -295,12 +295,13 @@ export default function Students() {
                     >
                       <th
                         scope="row"
-                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                       >
                         {student.name}
                       </th>
                       <td className="px-6 py-4">{student.age}</td>
-                      <td className="px-6 py-4">{getGender(student.gender)}</td>
+                      <td className="px-6 py-4">{student.address}</td>
+                      {/* <td className="px-6 py-4">{getGender(student.gender)}</td> */}
                       <td className="px-6 py-4">
                         <AppButton
                           className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
